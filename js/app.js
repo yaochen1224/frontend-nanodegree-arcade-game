d1b097771f5089b07f8aec01f2ab79ef4657b93f
yaochen1224 <yao.chen1224@foxmail.com>
2017-03-22 13:50:33 -0400

    saved

--- js/app.js
+++ js/app.js
@@ -27,7 +27,23 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y){
+  this.x = x;
+  this.y = y;
+  this.player = 'images/char-boy.png';
+}
 
+Player.prototype.handleInput = function(input){
+  if (input === 'left'){
+    this.x = 
+  }
+}
+
+Player.prototype.update = function(){}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
