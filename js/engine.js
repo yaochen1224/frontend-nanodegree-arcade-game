22a9ca76df9d305aadd5ba8ba94b5b1f7818c203
yaochen1224 <yao.chen1224@foxmail.com>
2017-03-23 08:55:16 -0400

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -29,7 +29,7 @@ var Engine = (function(global) {
     canvas.height = 606;
     doc.body.appendChild(canvas);
 
-    ctx.strokeRect(0,0,canvas.width, canvas.height);
+    // ctx.strokeRect(0,0,canvas.width, canvas.height);
 
 
     /* This function serves as the kickoff point for the game loop itself
@@ -115,10 +115,11 @@ var Engine = (function(global) {
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
+                'images/stone-block.png',
                 'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
-            numRows = 6,
+            numRows = 7,
             numCols = 5,
             row, col;
 
